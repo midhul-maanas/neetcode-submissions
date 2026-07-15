05685c5af5b0ea0db24c233d800b60dccf75114c
Midhul Maanas <midhulmaanas@gmail.com>
2026-07-15 13:55:23 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #fibonacci
+        
+        if n == 1 or n == 2:
+            return n
+        a,b = 1,2
+        climb = 0
+        for i in range(3,n + 1):
+            climb = a + b
+            a = b
+            b = climb
+        return climb
+            
